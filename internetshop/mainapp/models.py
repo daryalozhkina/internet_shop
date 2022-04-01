54f580e19cea84d8e7ad2b22e0b8d29ca3bd8cc2
ь <das.lozh19@mail.ru>
2022-04-01 21:25:37 +0500

    templates

--- internetshop/mainapp/models.py
+++ internetshop/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField('Название', max_length=60)
+    desc = models.TextField('Описание', blank=True)
+    # photo = models.ImageField('Фото', upload_to='images/', blank=True)
+    year = models.IntegerField('Год выпуска')
+    price = models.DecimalField('Цена', max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
--- internetshop/mainapp/views.py
+++ internetshop/mainapp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    pass
+    context = {
+        'page_title': 'главная'
+    }
+    return render(request, 'mainapp/index.html', context)
